b1259cd0847aa3d31c09cbdbffa7a2a429e5b0cd
golap-raj <golapraj47@gmail.com>
2025-03-05 15:56:36 +0600

    add var const let

---
+++ let-const-var.js
@@ -0,0 +1,25 @@
+// var diye diclar amra kokhono korbo nha at aekta old version
+
+// var name = "Golap";
+// console.log(name)
+
+// jodi kunu man change korte hoi taile let diye diclar korbo
+
+
+// let name = "Golap";
+// console.log(name);
+
+// name = "tahmid Hasan";
+// console.log(name)
+
+
+// jodi kuna man amra kokhono change na kori ta hole amra const diye diclar korbo
+
+// const name = "Tahmid Hasan";
+// console.log(name);
+
+
+// change korle aror asbe
+
+// name = "golap";
+// console.log(name)
