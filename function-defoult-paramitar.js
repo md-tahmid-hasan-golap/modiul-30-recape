ebb8cc85e59ea90c939721f87fc04ab0b3cbf7ba
golap-raj <golapraj47@gmail.com>
2025-03-05 17:54:42 +0600

    add string

---
+++ function-defoult-paramitar.js
@@ -0,0 +1,14 @@
+// function sum (num1=0, num2=0){
+//     const result = num1 + num2;
+//     console.log(result)
+// }
+
+// sum(19)
+
+
+// function multiple (num1=1, num2=1){
+//     const result = num1 * num2;
+//     console.log(result)
+
+// }
+// multiple(11)
---
+++ tamplate-string.js
@@ -0,0 +1,17 @@
+
+const country = "bangladesh";
+
+// const newString = "amadar ai " + country + " khub sundor"
+// console.log(newString)
+
+const newstring = `amadar ai ${country} khub sundor`
+console.log(newstring)
+
+
+const now = new Date();
+const hours = now.getHours();
+const minute = now.getMinutes();
+const secend = now.getSeconds();
+
+const time = `${hours}: ${minute}: ${secend}`;
+console.log(time)
